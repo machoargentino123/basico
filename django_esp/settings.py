b4d2bbef01f396a0fd0522a700ee2bc12360c6f9
machoargentino123 <nahuelirazausta@gmail.com>
2022-04-01 14:21:06 -0300

    `mensaje`

--- django_esp/settings.py
+++ django_esp/settings.py
@@ -25,9 +25,8 @@ SECRET_KEY = 'django-insecure-ak-t(u66bag@c%dn2)^y=pqscttk3fm5330k*#q(=!8-s@&_ph
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ruta = 'nahueltest.azurewebsites.net'
-ALLOWED_HOSTS = [ruta]
-CSRF_TRUSTED_ORIGINS = [ruta]
+ALLOWED_HOSTS = ['nahueltest.azurewebsites.net']
+CSRF_TRUSTED_ORIGINS = ['nahueltest.azurewebsites.net']
 
 # Application definition
 
